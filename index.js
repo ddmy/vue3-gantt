6447c679a3fd65adb5d9406cdb8dc0f1566937e4
YIjio <841312998@qq.com>
2022-08-11 15:26:39 +0800

    更改配置

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
- export { default } from './src/components/Gantt.vue'
+import Gantt from './src/components/Gantt.vue'
+import * as Util from './src/util/index.js'
+
+export default { Gantt }
+
+export { Util }
