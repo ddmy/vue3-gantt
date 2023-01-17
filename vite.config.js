543190fd42c461d1189468b436aab25425acd324
YIjio <841312998@qq.com>
2023-01-17 12:44:18 +0800

    优化配置

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,14 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ['vue', 'html2canvas', 'lodash', 'lay-excel'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
-          vue: 'Vue'
+          vue: 'Vue',
+          html2canvas: 'html2canvas',
+          lodash: 'lodash',
+          'lay-excel': 'lay-excel'
         }
       }
     },
