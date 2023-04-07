23990185103ff27ea75159d4801c255662b2ba4a
YIjio <841312998@qq.com>
2023-04-08 00:18:11 +0800

    色值转换优化

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,15 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue', 'html2canvas', 'lodash', 'lay-excel'],
+      external: ['vue', 'html2canvas', 'lodash', 'lay-excel', '@ctrl/tinycolor'],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
           html2canvas: 'html2canvas',
           lodash: 'lodash',
-          'lay-excel': 'lay-excel'
+          'lay-excel': 'lay-excel',
+          '@ctrl/tinycolor': '@ctrl/tinycolor'
         }
       }
     },
